462f16904b537c34603df968c950c8184f03ea8d
koafc <kfc@polygraphis.com>
2023-10-19 15:45:20 +0800

    app.py

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ import requests
 import json
 import time
 
-os.environ["REPLICATE_API_TOKEN"] = "r8_Fz9hzlCII6w406EbeJgQdfSD56Ma2W74HbqyF"
+os.environ["REPLICATE_API_TOKEN"] = "787f515cb0624813736c11e7fefec66473394f02"
 app = Flask(__name__)
 @app.route("/", methods=["GET","POST"])
 def index():
     if request.method == "POST":
         q = request.form.get("q")
         body = json.dumps({"version": "db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf", "input": { "prompt": q } })
-        headers = {'Authorization': 'Token r8_Fz9hzlCII6w406EbeJgQdfSD56Ma2W74HbqyF','Content-Type': 'application/json'}
+        headers = {'Authorization': 'Token 787f515cb0624813736c11e7fefec66473394f02','Content-Type': 'application/json'}
         output = requests.post('https://api.replicate.com/v1/predictions',data=body,headers=headers)
         time.sleep(10)
         get_url = output.json()['urls']['get']
