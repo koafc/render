bccef0dcac5f81e53b4067f407722504da6ad251
koafc <kfc@polygraphis.com>
2023-10-20 12:25:14 +0800

    app.py

--- app.py
+++ app.py
@@ -1,32 +1,37 @@
-from flask import Flask, render_template, request
-import os
-import requests
+from flask import Flask
+from flask import render_template,request
 import json
 import time
-import replicate
+import requests
 
-# os.environ["REPLICATE_API_TOKEN"] = "r8_8KikJnNAhhr7wVJtLBa0w0L7QvaACoM3ktx1J"
 app = Flask(__name__)
-@app.route("/", methods=["GET","POST"])
+
+headers = {
+    "Authorization" : "Token 787f515cb0624813736c11e7fefec66473394f02",
+    "Content-Type" : "application/json"
+}
+
+@app.route("/",methods=["GET","POST"])
 def index():
     if request.method == "POST":
-        q = request.form.get("q")
-        body = json.dumps({"version": "db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf", "input": { "prompt": q } })
-        headers = {"Authorization": "Token r8_8KikJnNAhhr7wVJtLBa0w0L7QvaACoM3ktx1J","Content-Type": "application/json"}
+        q = request.form.get("question")
+        print(q)
+        body = json.dumps(
+            {
+                "version" : "db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
+                "input"   :
+                {
+                    "prompt" : q
+                }
+            }
+        )
         output = requests.post("https://api.replicate.com/v1/predictions",data=body,headers=headers)
         time.sleep(10)
-        print(output.status_code)
-        # Print the content of the response
-        print(output.content)
-        print(os.environ.get("REPLICATE_API_TOKEN"))
-        get_url = output.json()['urls']['get']
-        print(output.json())
-        print(get_url)
+        get_url = output.json()["urls"]["get"]
         get_result = requests.post(get_url,headers=headers).json()['output']
-        print(get_result)
-        return(render_template("index.html", result=get_result[0]))
+        return(render_template("index.html",result=get_result[0]))
     else:
-        return(render_template("index.html", result="waiting"))
+        return(render_template("index.html",result="waiting........"))
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     app.run()
